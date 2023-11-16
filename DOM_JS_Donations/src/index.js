8ad5e2f3f951914b5ffc9208e5e4eccb93b0fd6e
Darwin Ameli <145455744+darwinamelihearc@users.noreply.github.com>
2023-11-16 13:25:54 +0100

    Ajout de ma solution

--- DOM_JS_Donations/src/index.js
+++ DOM_JS_Donations/src/index.js
@@ -1,2 +1,30 @@
 // votre code JS
+document.addEventListener("DOMContentLoaded", () => {
+  const addButton = document.querySelector(".btn-primary");
+  const inputField = document.getElementById("montant");
+  const donationsList = document.getElementById("donations");
+  const totalDisplay = document.querySelector(".display-5");
+  let total = 0;
 
+  // On ajoute un évènement lors d'un clic sur le bouton
+  addButton.addEventListener("click", () => {
+    // On récupère la valeur de l'utilisateur
+    const montant = parseFloat(inputField.value);
+
+    // Vérifier si le montant est un nombre
+    if (!isNaN(montant) && montant > 0) {
+      // Ajouter le montant à la liste
+      const newItem = document.createElement("li");
+      newItem.classList.add("list-group-item");
+      newItem.textContent = montant;
+      donationsList.appendChild(newItem);
+
+      // Mettre à jour et afficher le total
+      total += montant;
+      totalDisplay.textContent = `Total: ${total}`;
+
+      // Réinitialiser le champ de saisie
+      inputField.value = "";
+    }
+  });
+});
